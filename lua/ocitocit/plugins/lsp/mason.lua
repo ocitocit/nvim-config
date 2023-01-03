cbeab504370b04018f6fb71b8318d4e257367644
ocitocit <ocritocrit@gmail.com>
2023-01-03 16:09:33 +0700

    adding lsp plugin

--- lua/ocitocit/plugins/lsp/mason.lua
+++ lua/ocitocit/plugins/lsp/mason.lua
@@ -30,5 +30,5 @@ mason_lspconfig.setup({
     "emmet_ls",
   },
   -- auto-install configured servers (with lspconfig)
-  automatic_installation = true, -- not the same as ensure_installed
+  -- automatic_installation = true, -- not the same as ensure_installed
 })
