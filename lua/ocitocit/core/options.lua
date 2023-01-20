27de7060996fac6aae7047ac10b41daa6928c29f
ocitocit <ocritocrit@gmail.com>
2023-01-20 10:13:26 +0700

    someupdate

--- lua/ocitocit/core/options.lua
+++ lua/ocitocit/core/options.lua
@@ -11,7 +11,7 @@ opt.expandtab = true
 opt.autoindent = true
 
 -- line wrapping
-opt.wrap = false
+opt.wrap = true
 
 -- search settings
 opt.ignorecase = true
@@ -30,6 +30,8 @@ opt.backspace = "indent,eol,start"
 
 -- clipboard
 opt.clipboard:append("unnamedplus")
+
+
 
 -- split windows
 opt.splitright = true
